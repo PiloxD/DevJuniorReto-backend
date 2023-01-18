a5a89c536732b3d08c3559609234cffa554d3291
ADiaz <andres.diaz@sofka.com.co>
2023-01-18 15:56:32 -0500

    Se creó el proyecto en spring init, se configuraron todas las herramientas para trabajar java en VSC y se probó la conexión a la base de datos MongoDB Atlas

---
+++ backend/src/main/java/sofka/backend/Controller.java
@@ -0,0 +1,34 @@
+package sofka.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+
+@Configuration
+public class Controller {
+
+    private  ReactiveMongoTemplate template;
+
+    public Controller(ReactiveMongoTemplate template) {
+        this.template = template;
+    } 
+
+
+
+    @Bean
+    public RouterFunction<ServerResponse> createCategory() {
+        return route(
+                POST("/test/create/").and(accept(MediaType.APPLICATION_JSON)),
+                request -> template.save(request.bodyToMono(Model.class), "TESTDEPRUEBA")
+                        .then(ServerResponse.ok().build())
+        );
+    }
+    
+}
---
+++ backend/src/main/java/sofka/backend/Model.java
@@ -0,0 +1,16 @@
+package sofka.backend;
+
+
+public class Model {
+    private String texto;
+
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+}
